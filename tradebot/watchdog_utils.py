992933767fd4a533e0d7d7858234883161fbd99f
igmik <mikhalev.i@gmail.com>
2022-10-11 22:31:34 +0100

    Config file (#7)
    * Implement config file
    
    * Fix bugs
    
    * Add exception
    
    * Add watchdog process

---
+++ tradebot/watchdog_utils.py
@@ -0,0 +1,38 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import yaml
+from symbol import Symbol
+import os
+
+
+class FsHandler(FileSystemEventHandler):
+    def __init__(self, config_file, sess):
+        super().__init__()
+        self.sess = sess
+        self.config_file = config_file
+
+    def on_modified(self, event):
+        if self.config_file != event.src_path:
+            return
+
+        print(f'{event.event_type}  file: {event.src_path}')
+        try:
+            with open(self.config_file, "r") as file:
+                config = yaml.safe_load(file)
+            ref = config.get('global_reference', None)
+            symbols = {symbol_name: Symbol(symbol_name, symbol_config, ref=ref) for symbol_name, symbol_config in config['symbols'].items()}
+            self.sess.symbols = symbols
+            print(f"Apply new symbols config from {self.config_file}:\n{yaml.dump(config['symbols'], indent=4)}")
+
+        except Exception as e:
+            print(f"Failed to read and apply new config {self.config_file}")
+            print(e)
+            print('Skip.')
+            pass
+
+def start_config_watchdog(config_file, sess):
+    event_handler = FsHandler(config_file, sess)
+    observer = Observer()
+    dir_name = os.path.dirname(os.path.abspath(config_file))
+    observer.schedule(event_handler, path=dir_name, recursive=False)
+    observer.start()
