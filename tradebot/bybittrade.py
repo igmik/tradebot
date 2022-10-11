486a58167c9f074cd259bce9a1d685764c567967
igmik <mikhalev.i@gmail.com>
2022-10-11 20:20:20 +0100

    Revert "Config file (#4)"
    This reverts commit 98f2ff791519f78ca369ffe2dfcca251964f0976.

--- tradebot/bybittrade.py
+++ tradebot/bybittrade.py
@@ -1,93 +1,53 @@
 import math
-from symtable import Symbol
 from pybit import usdt_perpetual
-from collections import defaultdict
-
-
-ENDPOINT = 'https://api-testnet.bybit.com'
-
 
 class BybitTrade:
-    def __init__(self, api_key, api_secret, symbols:dict,
-            endpoint:str=ENDPOINT,
-            trade_type:str='usdt_perpetual',
+    def __init__(self, api_key, api_secret, approved_symbols:list,
+            leverage:int=1,
+            amount:int=100,
+            take_profit:int=4,
+            stop_loss:int=4,
+            open_policy:bool=False,
+            close_policy:int=0,
         ):
-
-        self.symbols = symbols
-        self.trade_type = trade_type
-        if trade_type == 'usdt_perpetual':
-            trade_api = usdt_perpetual
-
-        self.session = trade_api.HTTP(
-            endpoint=endpoint, 
+        self.session = usdt_perpetual.HTTP(
+            endpoint='https://api.bybit.com', 
             api_key=api_key,
             api_secret=api_secret,
         )
         
-        self.order_counter = {}
-        self.order_counter['Buy'] = defaultdict(list)
-        self.order_counter['Sell'] = defaultdict(list)
-        self._init_symbols(symbols)
+        self.amount = amount
+        self.take_profit = take_profit
+        self.stop_loss = stop_loss
+        self.open_policy = open_policy
+        self.close_policy = close_policy
+        self._init_symbols(leverage, approved_symbols)
     
-    def _init_symbols(self, symbols:dict) -> None:
-        if not symbols:
-            raise ValueError("Approved symbols configuration is mandatory, got None.")
+    def _init_symbols(self, leverage:int, approved_symbols:list) -> None:
+        if not approved_symbols:
+            raise ValueError("Approved symbols list is mandatory, got None.")
 
         res = self.session.query_symbol()
         self.ticks = {}
         self.sizes = {}
+        approved_symbols = set(approved_symbols)
         for symbol in res['result']:
-            if symbol['name'] not in symbols:
+            if symbol['name'] not in approved_symbols:
                 continue
-            
-            cur_symbol = symbols[symbol['name']]
 
             self.ticks[symbol['name']] = symbol['price_filter']
             self.sizes[symbol['name']] = symbol['lot_size_filter']
-            self.set_leverage(cur_symbol)
-    
-    def _get_current_price(self, symbol:str, side:str=None) -> float:
-        res = self.session.orderbook(symbol=symbol)
-        if not side:
-            side = 'Buy'
-        return float(res['result'][0]['price']) if side == "Buy" else float(res['result'][1]['price'])
 
-    def _apply_close_policy_1(self, position, symbol, target_profit=0.2, **kwargs):
-        _ = kwargs
-        if position['unrealised_pnl'] > target_profit:
-            print(f"Close {position['side']} {symbol} position with {position['unrealised_pnl']} profit")
-            return self.session.close_position(symbol)
-        return None
-    
-    def _apply_close_policy_2(self, position, symbol, side, **kwargs):
-        _ = kwargs
-        if position['side'] != side:
-            print(f"{symbol} is open for {position['side']}, but requested for {side}. Closing.")
-            return self.session.close_position(symbol)
-        return None
-    
-    def _apply_close_policy(self, close_policy:int, position:dict, **kwargs) -> object:
-        if position:
-            if close_policy == 1:
-                return self._apply_close_policy_1(position, **kwargs)
-            elif close_policy == 2:
-                return self._apply_close_policy_2(position, **kwargs)
-            else:
-                return None
-        else:
-            return True
-
-    def set_leverage(self, symbol:Symbol) -> None:
-        try:
-            self.session.set_leverage(
-                symbol=symbol.name,
-                buy_leverage=symbol.buy_leverage,
-                sell_leverage=symbol.sell_leverage,
-            )
-        except Exception as e:
-            print(e)
-            print("Skip")
-            pass
+            try:
+                self.session.set_leverage(
+                    symbol=symbol['name'],
+                    buy_leverage=leverage,
+                    sell_leverage=leverage,
+                )
+            except Exception as e:
+                print(e)
+                print("Skip")
+                pass
     
     def current_positions(self) -> list:
         result = self.session.my_position()['result']
@@ -103,49 +63,70 @@ class BybitTrade:
                 return pos
         return None
     
-    def close_opened_position(self, symbol, side):
-        try:
-            position = self.get_active_position(self.current_positions(), symbol)
+    def create_perp_orders_bulk(self, orders:list, order_type:str='Limit') -> None:
+        positions = self.current_positions()
+        for order in orders:
+            symbol = order['symbol']
+            side = order['side']
+            position = self.get_active_position(positions, symbol)
             if position:
                 if position['side'] == side:
-                    print(f"Closing {symbol} for side {side}.")
-                    self.session.close_position(symbol)
+                    if self.close_policy == 1 and position['unrealised_pnl'] > 0.2:
+                        print(f"Close {position['side']} {symbol} position with {position['unrealised_pnl']} profit")
+                        self.session.close_position(symbol)
+                    else:
+                        print(f"{side} order for {symbol} is already open")
+                        continue
                 else:
-                    print(f"Position {symbol} for side {side} not found.")
-                    print("Skip.")
+                    if self.close_policy == 0 and not self.open_policy:
+                        print(f"{symbol} is open for {position['side']}, but requested for {side}. Ignore.")
+                        continue
+                    elif self.close_policy == 1:
+                        if position['unrealised_pnl'] > 0.2:
+                            print(f"Close {position['side']} {symbol} position with {position['unrealised_pnl']} profit")
+                            self.session.close_position(symbol)
+                        else:
+                            print(f"{symbol} is open for {position['side']}, but requested for {side}. Ignore.")
+                            continue
+                    elif self.close_policy == 2:
+                        print(f"{symbol} is open for {position['side']}, but requested for {side}. Closing.")
+                        self.session.close_position(symbol)
+                    else:
+                        print("Open opposite position")
+
+            if 'amount' in order:
+                amount = order['amount']
             else:
-                print(f"Cannot close position {symbol} for side {side} as no opened positions.")
-                print("Skip.")
-        except Exception as e:
-            print(f"Failed to close order: {symbol} for side {side}.")
-            print(e)
-            print("Skip")
-            pass  
+                amount = self.amount
+            
+            self.create_perp_order(symbol, side, amount, order_type=order_type)
     
-    def close_last_order(self, symbol, side):
-        try:
-            if not self.order_counter[side][symbol]:
-                print(f"No active orders of {symbol} for side {side} to close")
-                print("Skip")
-                return
-    
-            order_id = self.order_counter[side][symbol].pop()
-            print(f"Closing {symbol} for side {side} with order {order_id}.")
-            self.session.close_order(symbol=symbol, parentOrderId=order_id)
-        except Exception as e:
-            print(f"Failed to close order: {symbol} for side {side} with order {order_id}.")
-            print(e)
-            print("Skip")
-            pass
-    
-    def adjust_tpsl(self, symbol, side, price, tp_perc, sl_perc):
+    def create_perp_order(self, symbol:str, side:str, amount:int=None, order_type:str='Limit', target_price:int=None, tp_perc:int=None, sl_perc:int=None) -> None:
+        amount = self.amount if not amount else amount
+        tp_perc = self.take_profit if not tp_perc else tp_perc
+        sl_perc = self.stop_loss if not sl_perc else sl_perc
+
+        if target_price:
+            price = target_price
+        else:
+            res = self.session.orderbook(symbol=symbol)
+            price = float(res['result'][0]['price']) if side == "Buy" else float(res['result'][1]['price'])
+        
+        # Round quantity up to a qty_step
+        #qty = amount / price
+        #factor = 1/float(self.sizes[symbol]['qty_step'])
+        #qty = round(qty*factor) / factor
+        qty = round(amount/price, 5)
+
+        
         if side == 'Buy':
             sl = price - price * sl_perc/100
             tp = price + price * tp_perc/100
         else:
             sl = price + price * sl_perc/100
             tp = price - price * tp_perc/100
-        
+
+        # Round sl/tp up to a tick_size
         tick_size = float(self.ticks[symbol]['tick_size'])
         factor = 1 / tick_size
         sl = math.floor(sl*factor)
@@ -158,56 +139,19 @@ class BybitTrade:
         # Really a hacky workaround to mitigate float representation error in IEEE-754. Propose better. TODO
         sl = round(sl / factor, 16)
         tp = round(tp / factor, 16)
-
-        return tp, sl
-    
-    def create_perp_order(self, symbol:str, side:str, qty:float=0.0, target_price:int=None) -> None:
-        size = self.symbols[symbol].size
-        tp_perc = self.symbols[symbol].take_profit
-        sl_perc = self.symbols[symbol].stop_loss
-        mp = self.symbols[symbol].multiplier
-        order_type = self.symbols[symbol].order_type
-        open_policy = self.symbols[symbol].open_policy
-        close_policy = self.symbols[symbol].close_policy
-        target_profit = self.symbols[symbol].target_profit
-        
-        if open_policy or close_policy:
-            position = self.get_active_position(self.current_positions(), symbol)
-            closed = self._apply_close_policy(close_policy, position, symbol=symbol, side=side, target_profit=target_profit) if close_policy else None
-            if position and not closed and not open_policy:
-                print(f"{position['side']} order for {symbol} is already open")
-                print('Skip')
-                return                  
-
-        if target_price:
-            price = target_price
-        else:
-            price = self._get_current_price(symbol, side)
-        
-        factor = 1/float(self.sizes[symbol]['qty_step'])
-        qty = qty * mp if qty else size / price
-        # Round quantity up to a qty_step
-        qty = math.floor(max(qty*factor, 1.0)) / factor
-
+                
         order = {}
         order['symbol'] = symbol
         order['side'] = side
         order['price'] = price
         order['qty'] = qty
+        order['stop_loss'] = sl
+        order['take_profit'] = tp
         order['order_type'] = order_type
-
-        tp, sl = None, None
-        if sl_perc or tp_perc:
-            tp, sl = self.adjust_tpsl(symbol, side, price, tp_perc, sl_perc)
-            tp = tp if tp_perc else None
-            sl = sl if sl_perc else None
-            order['stop_loss'] = sl
-            order['take_profit'] = tp
-        
         print(order)
 
         try:
-            resp = self.session.place_active_order(
+            self.session.place_active_order(
                 symbol=symbol,
                 side=side,
                 order_type=order_type,
@@ -219,10 +163,6 @@ class BybitTrade:
                 reduce_only=False,
                 close_on_trigger=False,
             )
-            if 'order_id' in resp['result'] or 'orderId' in resp['result']:
-                order_id = resp['result'].get('order_id') if 'order_id' in resp['result'] else resp['result'].get('orderId')
-                self.order_counter[side][symbol].append(order_id)
-                print(f"Opened {symbol} for {side} with order ID {order_id}")
         except Exception as e:
             print(f"Failed to create order: {order}")
             print(e)
